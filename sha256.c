adfc6d61fd897de03af7f15e1a38eb9df559f404
DuffyTJ89 <tomduffy89@gmail.com>
2019-02-25 16:56:09 +0000

    added most of algorithm, need to add Ch and Maj

--- sha256.c
+++ sha256.c
@@ -7,6 +7,13 @@
 
 void sha256();
 
+// see sections 4.1.2 for definitions
+uint32_t sig0(uint32_t x);
+uint32_t sig1(uint32_t x);
+
+// see section 3.2 for defintions
+uint32_t rotr(uint32_t n, uint32_t x);
+uint32_t shr(uint32_t n, uint32_t x);
 
 int main {int argc, char *argv[]){
 
@@ -30,6 +37,72 @@ void sha256(){
   , 0x9b05688c
   , 0x1f83d9ab
   , 0x5be0cd19 
- }; 
+ };
+
+ //the current message block
+ uint32_t M [16];
+
+ //for looping
+ int t;
+
+ //from page22, W[t] = M[t] for 0<= t >= 15
+ for (t=0; t<16;t++)
+    W[t] = M[t];
+
+ //from page22, W[t] = ... equation in 6.2.2 part 1
+ for (t=16; t<64; t++)
+    sig1(W[t-2] + W[t-7] + sig0(W[t-15]) + W[t-16];
+
+  //initialise a, b, c ... h as per step 2, page 22
+  a = H[0]; b = H[1]; c = H[2]; d = H[3]; e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+  //step 3
+  for (t=0; t<64; t++){
+    //see section 4.1.2 for Ch and Maj
+    T1 = h + SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
+    T2 = SIG_0(a) + Maj(a,b,c);
+    h = g;
+    g = f;
+    f = e;
+    e = d + T1;
+    d = c;
+    c = b;
+    b = a;
+    a = T1 + T2;
+
+
+   //step 4
+    H[0] = a + H[0];
+    H[1] = b + H[1];
+    H[2] = c + H[2];
+    H[3] = d + H[3];
+    H[4] = e + H[4];
+    H[5] = f + H[5];
+    H[6] = g + H[6];
+    H[7] = h + H[7];
+
+  }//end for
 
 }//end sha256()
+
+uint32_t rotr(uint32_t n, uint32_t x){
+ return (x >> n) | (x << (32 - n));
+
+}//end rotr
+uint32_t shr(uint32_t n, uint32_t x){
+ return (x >> n);
+
+}//end shr
+
+
+
+uint32_t sig0(uint32_t x){
+ //see section 3.2 and 4.1.2  for definitions
+ return (rotr(7, x) ^ rotr (18, x) ^ shr (3, x));
+}//end sig0
+uint32_t sig1(uint32_t x){
+ //see section 3.2 and 4.1.2  for definitions
+ return (rotr(17, x) ^ rotr (19, x) ^ shr (10, x));
+}//end sig1
+
+
